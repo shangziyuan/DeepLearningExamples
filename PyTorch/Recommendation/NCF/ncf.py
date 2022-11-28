e3767c84244c9fc713d599135c81f38205968811
Shang Ziyuan <happy.red.bear@gmail.com>
2022-11-28 19:54:26 -0400

    modified PyTorch ncf.py to file specified in ansible script

--- PyTorch/Recommendation/NCF/ncf.py
+++ PyTorch/Recommendation/NCF/ncf.py
@@ -14,7 +14,7 @@
 #
 # -----------------------------------------------------------------------
 #
-# Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2018, NVIDIA CORPORATION. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -42,33 +42,27 @@ import torch.nn as nn
 import utils
 import dataloading
 from neumf import NeuMF
-from feature_spec import FeatureSpec
-from neumf_constants import USER_CHANNEL_NAME, ITEM_CHANNEL_NAME, LABEL_CHANNEL_NAME
 
 import dllogger
 
 from apex.parallel import DistributedDataParallel as DDP
 from apex import amp
 
-
 def parse_args():
-    parser = ArgumentParser(description="Train a Neural Collaborative"
+    parser = ArgumentParser(description="Train a Nerual Collaborative"
                                         " Filtering model")
     parser.add_argument('--data', type=str,
-                        help='Path to the directory containing the feature specification yaml')
-    parser.add_argument('--feature_spec_file', type=str, default='feature_spec.yaml',
-                        help='Name of the feature specification file or path relative to the data directory.')
+                        help='Path to test and training data files')
     parser.add_argument('-e', '--epochs', type=int, default=30,
                         help='Number of epochs for training')
-    parser.add_argument('-b', '--batch_size', type=int, default=2 ** 20,
-                        help='Number of examples for each iteration. This will be divided by the number of devices')
-    parser.add_argument('--valid_batch_size', type=int, default=2 ** 20,
-                        help='Number of examples in each validation chunk. This will be the maximum size of a batch '
-                             'on each device.')
+    parser.add_argument('-b', '--batch_size', type=int, default=2**20,
+                        help='Number of examples for each iteration')
+    parser.add_argument('--valid_batch_size', type=int, default=2**20,
+                        help='Number of examples in each validation chunk')
     parser.add_argument('-f', '--factors', type=int, default=64,
                         help='Number of predictive factors')
     parser.add_argument('--layers', nargs='+', type=int,
-                        default=[256, 256, 128, 64],
+                        default=[2, 2, 1, 1],
                         help='Sizes of hidden layers for MLP')
     parser.add_argument('-n', '--negative_samples', type=int, default=4,
                         help='Number of negative examples per interaction')
@@ -89,13 +83,11 @@ def parse_args():
     parser.add_argument('--dropout', type=float, default=0.5,
                         help='Dropout probability, if equal to 0 will not use dropout at all')
     parser.add_argument('--checkpoint_dir', default='', type=str,
-                        help='Path to the directory storing the checkpoint file, '
-                             'passing an empty path disables checkpoint saving')
+                        help='Path to the directory storing the checkpoint file, passing an empty path disables checkpoint saving')
     parser.add_argument('--load_checkpoint_path', default=None, type=str,
                         help='Path to the checkpoint file to be loaded before training/evaluation')
     parser.add_argument('--mode', choices=['train', 'test'], default='train', type=str,
-                        help='Passing "test" will only run a single evaluation; '
-                             'otherwise, full training will be performed')
+                        help='Passing "test" will only run a single evaluation, otherwise full training will be performed')
     parser.add_argument('--grads_accumulated', default=1, type=int,
                         help='Number of gradients to accumulate before performing an optimization step')
     parser.add_argument('--amp', action='store_true', help='Enable mixed precision training')
@@ -105,7 +97,7 @@ def parse_args():
 
 
 def init_distributed(args):
-    args.world_size = int(os.environ.get('WORLD_SIZE', default=1))
+    args.world_size = int(os.environ['WORLD_SIZE'])
     args.distributed = args.world_size > 1
 
     if args.distributed:
@@ -124,57 +116,37 @@ def init_distributed(args):
         args.local_rank = 0
 
 
-def val_epoch(model, dataloader: dataloading.TestDataLoader, k, distributed=False, world_size=1):
+def val_epoch(model, x, y, dup_mask, real_indices, K, samples_per_user, num_user,
+              epoch=None, distributed=False):
     model.eval()
-    user_feature_name = dataloader.channel_spec[USER_CHANNEL_NAME][0]
-    item_feature_name = dataloader.channel_spec[ITEM_CHANNEL_NAME][0]
-    label_feature_name = dataloader.channel_spec[LABEL_CHANNEL_NAME][0]
+
     with torch.no_grad():
         p = []
-        labels_list = []
-        losses = []
-        for batch_dict in dataloader.get_epoch_data():
-            user_batch = batch_dict[USER_CHANNEL_NAME][user_feature_name]
-            item_batch = batch_dict[ITEM_CHANNEL_NAME][item_feature_name]
-            label_batch = batch_dict[LABEL_CHANNEL_NAME][label_feature_name]
-            prediction_batch = model(user_batch, item_batch, sigmoid=True).detach()
+        for u,n in zip(x,y):
+            p.append(model(u, n, sigmoid=True).detach())
 
-            loss_batch = torch.nn.functional.binary_cross_entropy(input=prediction_batch.reshape([-1]),
-                                                                  target=label_batch)
-            losses.append(loss_batch)
+        temp = torch.cat(p).view(-1,samples_per_user)
+        del x, y, p
 
-            p.append(prediction_batch)
-            labels_list.append(label_batch)
-
-        ignore_mask = dataloader.get_ignore_mask().view(-1, dataloader.samples_in_series)
-        ratings = torch.cat(p).view(-1, dataloader.samples_in_series)
-        ratings[ignore_mask] = -1
-        labels = torch.cat(labels_list).view(-1, dataloader.samples_in_series)
-        del p, labels_list
-
-        top_indices = torch.topk(ratings, k)[1]
-
-        # Positive items are always first in a given series
-        labels_of_selected = torch.gather(labels, 1, top_indices)
-        ifzero = (labels_of_selected == 1)
+        # set duplicate results for the same item to -1 before topk
+        temp[dup_mask] = -1
+        out = torch.topk(temp,K)[1]
+        # topk in pytorch is stable(if not sort)
+        # key(item):value(prediction) pairs are ordered as original key(item) order
+        # so we need the first position of real item(stored in real_indices) to check if it is in topk
+        ifzero = (out == real_indices.view(-1,1))
         hits = ifzero.sum()
-        ndcg = (math.log(2) / (torch.nonzero(ifzero)[:, 1].view(-1).to(torch.float) + 2).log_()).sum()
-        total_validation_loss = torch.mean(torch.stack(losses, dim=0))
-        #  torch.nonzero may cause host-device synchronization
+        ndcg = (math.log(2) / (torch.nonzero(ifzero)[:,1].view(-1).to(torch.float)+2).log_()).sum()
 
     if distributed:
-        torch.distributed.all_reduce(hits, op=torch.distributed.ReduceOp.SUM)
-        torch.distributed.all_reduce(ndcg, op=torch.distributed.ReduceOp.SUM)
-        torch.distributed.all_reduce(total_validation_loss, op=torch.distributed.ReduceOp.SUM)
-        total_validation_loss = total_validation_loss / world_size
+        torch.distributed.all_reduce(hits, op=torch.distributed.reduce_op.SUM)
+        torch.distributed.all_reduce(ndcg, op=torch.distributed.reduce_op.SUM)
 
-
-    num_test_cases = dataloader.raw_dataset_length / dataloader.samples_in_series
-    hr = hits.item() / num_test_cases
-    ndcg = ndcg.item() / num_test_cases
+    hr = hits.item() / num_user
+    ndcg = ndcg.item() / num_user
 
     model.train()
-    return hr, ndcg, total_validation_loss
+    return hr, ndcg
 
 
 def main():
@@ -187,22 +159,6 @@ def main():
                                 dllogger.StdOutBackend(verbosity=dllogger.Verbosity.VERBOSE)])
     else:
         dllogger.init(backends=[])
-
-    dllogger.metadata('train_throughput', {"name": 'train_throughput', 'unit': 'samples/s', 'format': ":.3e"})
-    dllogger.metadata('best_train_throughput', {'unit': 'samples/s'})
-    dllogger.metadata('mean_train_throughput', {'unit': 'samples/s'})
-    dllogger.metadata('eval_throughput', {"name": 'eval_throughput', 'unit': 'samples/s', 'format': ":.3e"})
-    dllogger.metadata('best_eval_throughput', {'unit': 'samples/s'})
-    dllogger.metadata('mean_eval_throughput', {'unit': 'samples/s'})
-    dllogger.metadata('train_epoch_time', {"name": 'train_epoch_time', 'unit': 's', 'format': ":.3f"})
-    dllogger.metadata('validation_epoch_time', {"name": 'validation_epoch_time', 'unit': 's', 'format': ":.3f"})
-    dllogger.metadata('time_to_target', {'unit': 's'})
-    dllogger.metadata('time_to_best_model', {'unit': 's'})
-    dllogger.metadata('hr@10', {"name": 'hr@10', 'unit': None, 'format': ":.5f"})
-    dllogger.metadata('best_accuracy', {'unit': None})
-    dllogger.metadata('best_epoch', {'unit': None})
-    dllogger.metadata('validation_loss', {"name": 'validation_loss', 'unit': None, 'format': ":.5f"})
-    dllogger.metadata('train_loss', {"name": 'train_loss', 'unit': None, 'format': ":.5f"})
 
     dllogger.log(data=vars(args), step='PARAMETER')
 
@@ -220,22 +176,25 @@ def main():
 
     main_start_time = time.time()
 
-    feature_spec_path = os.path.join(args.data, args.feature_spec_file)
-    feature_spec = FeatureSpec.from_yaml(feature_spec_path)
-    trainset = dataloading.TorchTensorDataset(feature_spec, mapping_name='train', args=args)
-    testset = dataloading.TorchTensorDataset(feature_spec, mapping_name='test', args=args)
-    train_loader = dataloading.TrainDataloader(trainset, args)
-    test_loader = dataloading.TestDataLoader(testset, args)
+    train_ratings = torch.load(args.data+'/train_ratings.pt', map_location=torch.device('cuda:{}'.format(args.local_rank)))
+    test_ratings = torch.load(args.data+'/test_ratings.pt', map_location=torch.device('cuda:{}'.format(args.local_rank)))
+    test_negs = torch.load(args.data+'/test_negatives.pt', map_location=torch.device('cuda:{}'.format(args.local_rank)))
+
+    valid_negative = test_negs.shape[1]
+
+    nb_maxs = torch.max(train_ratings, 0)[0]
+    nb_users = nb_maxs[0].item() + 1
+    nb_items = nb_maxs[1].item() + 1
+
+    all_test_users = test_ratings.shape[0]
+
+    test_users, test_items, dup_mask, real_indices = dataloading.create_test_data(test_ratings, test_negs, args)
 
     # make pytorch memory behavior more consistent later
     torch.cuda.empty_cache()
 
     # Create model
-    user_feature_name = feature_spec.channel_spec[USER_CHANNEL_NAME][0]
-    item_feature_name = feature_spec.channel_spec[ITEM_CHANNEL_NAME][0]
-    label_feature_name = feature_spec.channel_spec[LABEL_CHANNEL_NAME][0]
-    model = NeuMF(nb_users=feature_spec.feature_spec[user_feature_name]['cardinality'],
-                  nb_items=feature_spec.feature_spec[item_feature_name]['cardinality'],
+    model = NeuMF(nb_users, nb_items,
                   mf_dim=args.factors,
                   mlp_layer_sizes=args.layers,
                   dropout=args.dropout)
@@ -243,7 +202,7 @@ def main():
     optimizer = FusedAdam(model.parameters(), lr=args.learning_rate,
                           betas=(args.beta1, args.beta2), eps=args.eps)
 
-    criterion = nn.BCEWithLogitsLoss(reduction='none')  # use torch.mean() with dim later to avoid copy to host
+    criterion = nn.BCEWithLogitsLoss(reduction='none') # use torch.mean() with dim later to avoid copy to host
     # Move model and loss to GPU
     model = model.cuda()
     criterion = criterion.cuda()
@@ -257,58 +216,48 @@ def main():
 
     local_batch = args.batch_size // args.world_size
     traced_criterion = torch.jit.trace(criterion.forward,
-                                       (torch.rand(local_batch, 1), torch.rand(local_batch, 1)))
+                                       (torch.rand(local_batch,1),torch.rand(local_batch,1)))
 
     print(model)
     print("{} parameters".format(utils.count_parameters(model)))
 
     if args.load_checkpoint_path:
         state_dict = torch.load(args.load_checkpoint_path)
-        state_dict = {k.replace('module.', ''): v for k, v in state_dict.items()}
+        state_dict = {k.replace('module.', '') : v for k,v in state_dict.items()}
         model.load_state_dict(state_dict)
 
     if args.mode == 'test':
         start = time.time()
-        hr, ndcg, val_loss = val_epoch(model, test_loader, args.topk,
-                                       distributed=args.distributed, world_size=args.world_size)
+        hr, ndcg = val_epoch(model, test_users, test_items, dup_mask, real_indices, args.topk,
+                             samples_per_user=valid_negative + 1,
+                             num_user=all_test_users, distributed=args.distributed)
         val_time = time.time() - start
-        eval_size = test_loader.raw_dataset_length
+        eval_size = all_test_users * (valid_negative + 1)
         eval_throughput = eval_size / val_time
 
-        dllogger.log(step=tuple(), data={'best_eval_throughput': eval_throughput,
-                                         'hr@10': hr,
-                                         'validation_loss': float(val_loss.item())})
+        dllogger.log(step=tuple(), data={'best_eval_throughput' : eval_throughput,
+                                         'hr@10' : hr})
         return
-
-    # this should always be overridden if hr>0.
-    # It is theoretically possible for the hit rate to be zero in the first epoch, which would result in referring
-    # to an uninitialized variable.
+    
     max_hr = 0
     best_epoch = 0
-    best_model_timestamp = time.time()
     train_throughputs, eval_throughputs = [], []
 
     for epoch in range(args.epochs):
 
         begin = time.time()
-        batch_dict_list = train_loader.get_epoch_data()
-        num_batches = len(batch_dict_list)
+
+        epoch_users, epoch_items, epoch_label = dataloading.prepare_epoch_train_data(train_ratings, nb_items, args)
+        num_batches = len(epoch_users)
         for i in range(num_batches // args.grads_accumulated):
             for j in range(args.grads_accumulated):
                 batch_idx = (args.grads_accumulated * i) + j
-                batch_dict = batch_dict_list[batch_idx]
+                user = epoch_users[batch_idx]
+                item = epoch_items[batch_idx]
+                label = epoch_label[batch_idx].view(-1,1)
 
-                user_features = batch_dict[USER_CHANNEL_NAME]
-                item_features = batch_dict[ITEM_CHANNEL_NAME]
-
-                user_batch = user_features[user_feature_name]
-                item_batch = item_features[item_feature_name]
-
-                label_features = batch_dict[LABEL_CHANNEL_NAME]
-                label_batch = label_features[label_feature_name]
-
-                outputs = model(user_batch, item_batch)
-                loss = traced_criterion(outputs, label_batch.view(-1, 1)).float()
+                outputs = model(user, item)
+                loss = traced_criterion(outputs, label).float()
                 loss = torch.mean(loss.view(-1), 0)
 
                 if args.amp:
@@ -321,34 +270,31 @@ def main():
             for p in model.parameters():
                 p.grad = None
 
-        del batch_dict_list
+        del epoch_users, epoch_items, epoch_label
         train_time = time.time() - begin
         begin = time.time()
 
-        epoch_samples = train_loader.length_after_augmentation
+        epoch_samples = len(train_ratings) * (args.negative_samples + 1)
         train_throughput = epoch_samples / train_time
         train_throughputs.append(train_throughput)
 
-        hr, ndcg, val_loss = val_epoch(model, test_loader, args.topk,
-                                       distributed=args.distributed, world_size=args.world_size)
+        hr, ndcg = val_epoch(model, test_users, test_items, dup_mask, real_indices, args.topk,
+                             samples_per_user=valid_negative + 1,
+                             num_user=all_test_users, epoch=epoch, distributed=args.distributed)
 
         val_time = time.time() - begin
-        eval_size = test_loader.raw_dataset_length
+
+
+        eval_size = all_test_users * (valid_negative + 1)
         eval_throughput = eval_size / val_time
         eval_throughputs.append(eval_throughput)
 
-        if args.distributed:
-            torch.distributed.all_reduce(loss, op=torch.distributed.ReduceOp.SUM)
-            loss = loss / args.world_size
-
         dllogger.log(step=(epoch,),
-                     data={'train_throughput': train_throughput,
-                           'hr@10': hr,
-                           'train_epoch_time': train_time,
-                           'validation_epoch_time': val_time,
-                           'eval_throughput': eval_throughput,
-                           'validation_loss': float(val_loss.item()),
-                           'train_loss': float(loss.item())})
+                     data = {'train_throughput': train_throughput,
+                             'hr@10': hr,
+                             'train_epoch_time': train_time,
+                             'validation_epoch_time': val_time,
+                             'eval_throughput': eval_throughput})
 
         if hr > max_hr and args.local_rank == 0:
             max_hr = hr
@@ -373,9 +319,7 @@ def main():
                            'best_accuracy': max_hr,
                            'best_epoch': best_epoch,
                            'time_to_target': time.time() - main_start_time,
-                           'time_to_best_model': best_model_timestamp - main_start_time,
-                           'validation_loss': float(val_loss.item()),
-                           'train_loss': float(loss.item())},
+                           'time_to_best_model': best_model_timestamp - main_start_time},
                      step=tuple())
 
 
